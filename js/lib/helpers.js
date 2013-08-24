8df82310fa043a3a9381d40b63bf7cf9a95108d4
David <davvblack@gmail.com>
2013-08-24 12:50:04 -0400

    Added Array.prototype.contains because fuck hygiene.

--- js/lib/helpers.js
+++ js/lib/helpers.js
@@ -44,3 +44,13 @@ function approach (current, goal, delta) {
 
     return current
 }
+
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] == obj) {
+            return true;
+        }
+    }
+    return false;
+}
