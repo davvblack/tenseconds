59af9b2be0a32ac620a976b1c2b28c43a3f311e2
David <davvblack@gmail.com>
2013-08-25 20:53:32 -0400

    Fighter sample CSV. Top line is the player, rest is going to be consecutive enemies to fight.

---
+++ data/fighter_definitions.data.js
@@ -0,0 +1,4 @@
+var fighter_definitions = [
+["default,",100,100,0,20,20,2,10,0,-0.125,0.4,0.5,0,0.6,0.4,0,","],
+["Old Man,",40,40,0,20,20,2,10,0,-0.125,0.6,0,0,1,0,0,"The old man down the street can barely lift his sword, yet still challenges you to a duel.  Use low block (z-/) to easily take no damage from him, then high attack (1-0) to kill him."]
+]
